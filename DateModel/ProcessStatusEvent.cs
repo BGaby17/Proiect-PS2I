6e6e7fd4ad2da5e8ab242364fa6a614ca1493443
andrei1245 <dodea.andrei04@gmail.com>
2025-05-18 21:35:03 +0300

    monitorizare buna

--- DateModel/ProcessStatusEvent.cs
+++ DateModel/ProcessStatusEvent.cs
@@ -4,14 +4,22 @@ namespace DateModel
 {
     public class ProcessStatusEvent
     {
+        // Constructor principal
         public ProcessStatusEvent(ProcessState NewInfo, DateTime StateChangedDate)
         {
             State = NewInfo;
             this.StateChangedDate = StateChangedDate;
+            Id = Guid.NewGuid(); // Generăm un ID unic
         }
 
-        public ProcessStatusEvent() { }
+        // Constructor implicit
+        public ProcessStatusEvent()
+        {
+            Id = Guid.NewGuid(); // Generăm un ID unic
+        }
 
+        // Proprietăți
+        public Guid Id { get; private set; } // ID unic generat automat
         public ProcessState State { get; set; }
         public DateTime StateChangedDate { get; set; }
     }
